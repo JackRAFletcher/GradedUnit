caee81d86264c743f461dc1eb130a07a9bd64e46
Jack Fletcher <jackrafletcher@gmail.com>
2017-04-18 09:46:41 +0100

    Edited connection string to change the name of the DB.
    Removed un-neccesary files; AssembyInfo.cs, app_data folder, favicon.ico & project_readme.html

---
+++ Property.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Property.Web.Startup))]
+namespace Property.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
